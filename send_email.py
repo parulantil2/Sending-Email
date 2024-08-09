ded123ead6d25984dcc7fe09738e8e193a46e667
parulantil2 <antilparul06@gmail.com>
2024-08-09 12:06:39 +0530

    Create send_email.py

---
+++ send_email.py
@@ -0,0 +1,37 @@
+# Sending mail using smtp server
+
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+# Email configuration
+smtp_server = 'smtp.gmail.com'
+smtp_port = 587
+username = 'antilparul06@gmail.com'
+password = 'wlix nmym sybi anzc'
+
+# Create the email content
+subject = 'Subject of the Email'
+body = 'This is the body of the email.'
+
+msg = MIMEMultipart()
+msg['From'] = username
+msg['To'] = 'antilparul06@gmail.com'
+msg['Subject'] = subject
+
+# Attach the email body
+msg.attach(MIMEText(body, 'plain'))
+
+# Connect to the SMTP server and send the email
+try:
+    server = smtplib.SMTP(smtp_server, smtp_port)
+    server.starttls()  # Secure the connection with TLS
+    server.login(username, password)
+    server.send_message(msg)
+    print('Email sent successfully!')
+
+except Exception as e:
+    print(f'Failed to send email: {e}')
+
+finally:
+    server.quit()
